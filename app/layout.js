623ecd2ef8c70dc5987f0fd6e18a4d519538bcc9
palenquiana <ludmilapalenque@hotmail.com>
2022-06-02 20:55:19 -0300

    feat: titulo y botones

---
+++ app/layout.js
@@ -0,0 +1,5 @@
+var header = document.createElement('header');
+var main = document.createElement('main');
+document.body.appendChild(header);
+document.body.appendChild(main);
+main.classList.add("bg-secondary", "bg-opacity-10");
