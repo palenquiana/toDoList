c13ef43942dd18deabfb49f09a727912d1b0b04a
palenquiana <ludmilapalenque@hotmail.com>
2022-06-02 20:20:30 -0300

    feat: files

---
+++ app/layout.ts
@@ -0,0 +1,6 @@
+
+const header = document.createElement('header');
+const main = document.createElement('main');
+document.body.appendChild(header);
+document.body.appendChild(main);
+main.classList.add("bg-secondary","bg-opacity-10");
